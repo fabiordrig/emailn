e3213225b9f1140766943d1fbc0b0033479a51b0
Fabio Rodrigues <fabiordrig@gmail.com>
2024-03-19 17:22:28 -0300

    feat: add content for the emails

--- infra/mail/send-mail.go
+++ infra/mail/send-mail.go
@@ -1,26 +1,29 @@
 package mail
 
 import (
+	"emailn/domains/campaign"
 	"os"
 
 	"gopkg.in/gomail.v2"
 )
 
-func SendEmail() {
+func SendEmail(campaign *campaign.Campaign) error {
 	m := gomail.NewMessage()
 
 	d := gomail.NewDialer(os.Getenv("MAIL_HOST"), 587, os.Getenv("MAIL_USER"), os.Getenv("MAIL_PASSWORD"))
 
-	m.SetHeader("From", os.Getenv("MAIL_USER"))
-	m.SetHeader("To", "fabiordrig@gmail.com")
+	var emails []string
 
-	m.SetBody("text/html", "Hello <b>World</b>")
-
-	err := d.DialAndSend(m)
-
-	if err != nil {
-		println("Error sending email", err.Error())
-		panic(err)
+	for _, contact := range campaign.Contacts {
+		emails = append(emails, contact.Email)
 	}
+
+	m.SetHeader("From", os.Getenv("MAIL_USER"))
+	m.SetHeader("Subject", campaign.Name)
+	m.SetHeader("To", emails...)
+
+	m.SetBody("text/html", campaign.Content)
+
+	return d.DialAndSend(m)
 
 }
